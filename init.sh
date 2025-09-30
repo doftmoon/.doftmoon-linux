b75105e77d4dfd4cb41d9247fa8c69a8f1f4f746
doftmoon <moistrab@gmail.com>
2025-09-30 12:19:21 +0300

    feat: small fixes and improvements

--- init.sh
+++ init.sh
@@ -1,55 +1,45 @@
 #!/usr/bin/env bash
 # Script to install all dep and automate init process
-# NOTICE: run only 1 "one" time!!! Rn have not enough time to test everything :3
 set -euo pipefail
+source ./libs/doftmoon-shell-utils
+# info()
+# error()
+# ensure_not_root()
+# detect_package_manager()
 
 USER='doftmoon'
-GIT_REPO='https://github.com/doftmoon/.doftmoon-linux.git'
-PKG_MANAGER='unknown'
-
-source ./custom_libs/doftmoon-shell-utils
-
-get_sudo_session() {
-   sudo -v
-   while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-}
-
-detect_package_manager() {
-   info 'Detecting package manager...'
-   if command -v apt-get &> /dev/null; then
-      PKG_MANAGER='apt'
-   elif command -v pacman &> /dev/null; then
-      PKG_MANAGER='pacman'
-   elif command -v dnf &> /dev/null; then
-      PKG_MANAGER='dnf'
-   elif command -v apk &> /dev/null; then
-      PKG_MANAGER='apk'
-   else
-      error 'Could not detect a known package manager. Aborting.'
-   fi
-
-   info "Detected package manager: $PKG_MANAGER"
-   sleep 1
-}
+GIT_REPO="https://github.com/$USER/.$USER-linux.git"
+PKG_MANAGER=$(detect_package_manager)
 
 install_dependencies() {
    local packages_apt='build-essential file curl git pipx'
    local packages_pacman='base-devel file curl git python-pipx'
+   local packages_dnf=''
+   local packages_apk=''
 
-   info 'Installing dependencies via package manager.'
-   if [ "$PKG_MANAGER" = 'apt' ]; then
-      sudo apt update
-      sudo apt upgrade
-      sudo apt install $packages_apt
-      sudo apt autoremove
-   elif [ "$PKG_MANAGER" = 'pacman' ]; then
-      sudo pacman -Syu --noconfirm
-      sudo pacman -S --noconfirm $packages_pacman
-   elif [ "$PKG_MANAGER" = 'dnf' ]; then
-      error 'Fedora is not supported now!'
-   elif [ "$PKG_MANAGER" = 'apk' ]; then
-      error 'Alpine is not supported now!'
-   fi
+   info 'Installing dependencies via package manager...'
+   case "$PKG_MANAGER" in
+      'apt')
+         sudo apt update
+         sudo apt upgrade
+         sudo apt install $packages_apt
+         sudo apt autoremove
+         ;;
+      'pacman')
+         sudo pacman -Syu --noconfirm
+         sudo pacman -S --noconfirm $packages_pacman
+         ;;
+      'dnf')
+         error 'Fedora is not supported now! Aborting.'
+         ;;
+      'apk')
+         error 'Alpine is not supported now! Aborting.'
+         ;;
+      *)
+         error 'Unknown package manager. Aborting.'
+         ;;
+   esac
+   info 'Successfully installed!'
 }
 
 install_ansible() {
@@ -57,15 +47,16 @@ install_ansible() {
    if ! command -v ansible &> /dev/null; then
       pipx install ansible
       sudo pipx ensurepath --global
+      info 'Ansible successfully installed!'
    else
-      info 'Ansible is already installed.'
+      info 'Ansible is already installed!'
    fi
 }
 
 ensure_latest_repo() {
    info 'Checking if git repo is up to date...'
    if [[ $(git remote show) = 'origin' ]]; then
-      git pull origin master
+      git pull origin
    else
       git remote add origin $GIT_REPO
       git pull origin master
@@ -74,16 +65,14 @@ ensure_latest_repo() {
 
 run_ansible() {
    info 'Starting ansible play...'
-   (cd ansible && ~/.local/bin/ansible-playbook -K playbook.yaml -e "github_user=$USER")
+   (cd ansible && ~/.local/bin/ansible-playbook -K --check playbook.yaml -e "github_user=$USER")
 }
 
 main() {
    ensure_not_root # ensures the script is run as a normal user with sudo privileges
-   get_sudo_session # get the sudo privileges for the script
-   detect_package_manager
    install_dependencies
    install_ansible
-   ensure_latest_repo
+   #ensure_latest_repo
    run_ansible
    info 'Bootstrap complete!'
 }
