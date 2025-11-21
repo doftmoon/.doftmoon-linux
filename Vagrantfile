ad00e6475ea034578cf36b53e8ea8465731bc17f
doftmoon <moistrab@gmail.com>
2025-11-21 19:48:42 +0300

    feat: huge update, well be finished soon

---
+++ Vagrantfile
@@ -0,0 +1,48 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2025 doftmoon
+
+Vagrant.configure("2") do |config|
+
+  config.vm.synced_folder ".", "/home/vagrant/.doftmoon-linux", disabled: false
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.gui = false
+    vb.memory = "2024"
+    vb.cpus = 2
+  end
+
+  config.vm.define "arch" do |arch|
+    arch.vm.box = "generic/arch"
+    arch.vm.box_version = "4.3.12"
+
+    arch.vm.hostname = "arch-test"
+
+    arch.vm.provision "shell", inline: <<-SHELL
+      sudo pacman -Syu --noconfirm
+      sudo pacman -S --noconfirm git
+    SHELL
+  end
+
+  config.vm.define "fedora" do |fedora|
+    fedora.vm.box = "generic/fedora39"
+
+    fedora.vm.hostname = "fedora-test"
+
+    fedora.vm.provision "shell", inline: <<-SHELL
+      sudo dnf update -y
+    SHELL
+  end
+
+  config.vm.define "ubuntu" do |ub|
+    ub.vm.box = "ubuntu/jammy64"
+
+    ub.vm.hostname = "ubuntu-test"
+
+    ub.vm.provision "shell", inline: <<-SHELL
+      sudo apt-get update -y
+      sudo apt-get install git -y
+    SHELL
+  end
+end
--- init.sh
+++ init.sh
@@ -1,52 +1,117 @@
 #!/usr/bin/env bash
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2025 doftmoon
+#
 # Script to install all dep and automate init process
 set -euo pipefail
-source ./libs/doftmoon-shell-utils
-# info()
-# error()
-# ensure_not_root()
-# detect_package_manager()
 
 USER='doftmoon'
+CURRENT_USER=$(whoami)
+
+WORK_HOST='ThinkPad'
+
+PROFILE=''
+
 GIT_REPO="https://github.com/$USER/.$USER-linux.git"
-PKG_MANAGER=$(detect_package_manager)
 
-install_dependencies() {
+ensure_not_root() {
+   if [[ "$EUID" -eq 0 ]]; then
+      error "Don't run as root!"
+   fi
+}
+
+info() {
+   if command -v gum &>/dev/null; then
+      gum style --foreground 10 "$1"
+   else
+      echo -e "\e[32mINFO:\e[0m $1"
+   fi
+}
+
+warn() {
+   if command -v gum &>/dev/null; then
+      gum style --foreground 11 "$1"
+   else
+      echo -e "\e[33mWARNING:\e[0m $1"
+   fi
+}
+
+error() {
+   if command -v gum &>/dev/null; then
+      gum style --foreground 9 "$1" >&2
+   else
+      echo -e "\e[31mERROR:\e[0m $1" >&2
+   fi
+   exit 1
+}
+
+confirm() {
+   if command -v gum &>/dev/null; then
+      gum confirm "$1"
+   else
+      echo -n "$1 (y/n): "; read -r r; [[ "$r" =~ [Yy]$ ]]
+   fi
+}
+
+choose() {
+   local prompt="$1"
+   shift
+   if command -v gum &>/dev/null; then
+      gum choose --cursor.foreground 12 --header="$prompt" --header.foreground 12 "$@"
+   elif command -v fzf &>/dev/null; then
+      printf "%s\n" "$@" |
+         fzf --prompt="$prompt" \
+            --height=20% \
+            --border --cycle
+   else
+      select opt in "$@"; do [[ -n "$opt" ]] && { echo "$opt"; break; }; done
+   fi
+}
+
+install_deps() {
+   local packages_pacman='base-devel file curl git python-pipx gum'
+   local packages_zypper='gum'
+   local packages_dnf='gum'
    local packages_apt='build-essential file curl git pipx'
-   local packages_pacman='base-devel file curl git python-pipx'
-   local packages_dnf=''
-   local packages_apk=''
+   local mgrs=(pacman dnf zypper apt)
+   local mgr=$(for m in ${mgrs[@]}; do command -v $m &>/dev/null && { echo ${m%%-*}; break; }; done)
+   info "Package manager: $mgr"
 
-   info 'Installing dependencies via package manager...'
-   case "$PKG_MANAGER" in
-      'apt')
-         sudo apt update
-         sudo apt upgrade
-         sudo apt install $packages_apt
+   case $mgr in
+      pacman) sudo pacman -S $packages_pacman --needed --noconfirm ;;
+      dnf) sudo dnf install $packages_dnf -y ;;
+      zypper) sudo zypper install gum -y ;;
+      # refrence https://github.com/basecamp/omakub/issues/222
+      apt)
+         sudo apt update -y
+         sudo apt upgrade -y
+         sudo apt install $packages_apt -y
+         if ! command -v gum &>/dev/null; then
+            warn "In order to install gum and lots of other modern terminal tools"
+            warn "The Charm repository needs to be added for apt in order to get package"
+            warn "this repo and Charm company is a well-known source of high-quality tools"
+            if confirm "Do you want to add Charm repository for apt?"; then
+               info "Adding Charm repository for apt..."
+               sudo mkdir -p /etc/apt/keyrings
+               curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+               echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+               sudo apt install gum -y
+            else
+               warn "Canceled operation"
+            fi
+         fi
          sudo apt autoremove
          ;;
-      'pacman')
-         sudo pacman -Syu --noconfirm
-         sudo pacman -S --noconfirm $packages_pacman
-         ;;
-      'dnf')
-         error 'Fedora is not supported now! Aborting.'
-         ;;
-      'apk')
-         error 'Alpine is not supported now! Aborting.'
-         ;;
-      *)
-         error 'Unknown package manager. Aborting.'
-         ;;
+      *) error "Cannot install dependecies automatically" ;;
    esac
-   info 'Successfully installed!'
+   info 'Dependencies installed'
 }
 
 install_ansible() {
    info 'Checking for ansible installation...'
-   if ! command -v ansible &> /dev/null; then
+   if ! command -v ansible &>/dev/null; then
       pipx install ansible
-      sudo pipx ensurepath --global
+      pipx ensurepath
       info 'Ansible successfully installed!'
    else
       info 'Ansible is already installed!'
@@ -60,19 +125,32 @@ ensure_latest_repo() {
    else
       git remote add origin $GIT_REPO
       git pull origin master
+      git checkout master
+   fi
+}
+
+check_user() {
+   if [[ "$USER" != "$CURRENT_USER" ]]; then
+      warn "The user that is running this bootstrap is different to creator $USER"
+   else
+      info "New installation, Master? >_<"
+      info "or...you forgot that everything already have been installed..."
    fi
 }
 
 run_ansible() {
    info 'Starting ansible play...'
-   (cd ansible && ~/.local/bin/ansible-playbook -K --check playbook.yaml -e "github_user=$USER")
+   (cd ansible && ~/.local/pipx/venvs/ansible/bin/ansible-playbook -K playbook.yaml)
 }
 
 main() {
-   ensure_not_root # ensures the script is run as a normal user with sudo privileges
-   install_dependencies
+   ensure_not_root
+   install_deps
    install_ansible
-   #ensure_latest_repo
+   ensure_latest_repo
+   check_user
+   #choose_installation
+   #check_device
    run_ansible
    info 'Bootstrap complete!'
 }
